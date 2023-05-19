c9d4135e73842b8843860e903206e14daebb182e
Shaywhytee <swensen42@gmail.com>
2023-05-19 14:42:47 -0600

    deploying backend

--- alex_back/app.py
+++ alex_back/app.py
@@ -357,4 +357,5 @@ def edit_content():
     return jsonify("Content Has Been Updated!")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
